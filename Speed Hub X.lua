b0d2e1f71c8fe2949a69ab00080636adf40a642a
VantaBlack122 <galactixyt9@gmail.com>
2024-12-09 22:04:44 +0400

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,6 +1,11 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
 for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
+    if PlaceID =-- Speed Hub X
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+
+-- Speed Hub X + ad pop up closer without delay
+loadstring(game:HttpGet("https://raw.githubusercontent.com/chiper1z/Speed-Hub-X-with-Ad-Bypasser/refs/heads/main/Protected_6424292347898505.txt"))()
+= game.PlaceId then
         loadstring(game:HttpGet(Execute))()
     end
 end
